30ec36e375fbef7a8a20ce359f30015de5b66560
d87zhang <dzhang323@hotmail.com>
2016-11-15 20:51:46 -0500

    [NEEDA TEST] Add password authentication code on backend
    Also refactored backend code quite a bit. Old functionality works.

--- BoardDirector.js
+++ BoardDirector.js
@@ -1,3 +1,9 @@
+function isValidPassword(candidatePassword) {
+    return candidatePassword.length >= 8;
+}
+
+
+
 module.exports = {
     // constructor function
     // a BoardDirector encapsulates the back-end code and state of a board with the
@@ -8,42 +14,89 @@ module.exports = {
 
         this.nextClientId = 0;
 
-        // state about the current drawing (TODO)
+        this.password = undefined;      // a password value of null or undefined
+                                        // means the board is not password-protected
+
+        // state about the current drawing
         // an object used as a map associating client ids to arrays of data_point
         // objects.
-        var drawingData = {};
+        this.drawingData = {};
 
-        // persists current state to the database
-        this.saveToDB = function() {} // TODO implement!
-        // populate current state with data from the database
-        this.loadFromDB = function() {} // TODO implement!
+        ///////////////////// method definitions start ///////////////////
 
-        var boardDirector = this;       // allows easier access to this in nested
-                                        // function scope below
-        // initialization
-        this.boardNameSpace.on('connection', function(socket){
-            console.log('someone connected on board ' + boardId);
-            var clientId = boardDirector.nextClientId;
-            boardDirector.nextClientId++;
+        // verify the user connected through socket;
+        // If verification is successful, then access is granted to the user
+        this.verifyUser = function(socket) {
+            if (typeof(this.password) === 'undefined') {
+                this.grantAccessToUser(socket);
+                return;
+            }
 
-            drawingData[clientId] = new Array();
+            var boardDirector = this;
+            socket.on('verify with password', function(password) {
+                if (password === boardDirector.password) {
+                    boardDirector.grantAccessToUser(socket);
+                    return;
+                }
+
+                socket.emit('incorrect password');
+            });
+
+            socket.emit('password required');
+        };
+
+        // grant board access to the user connected through socket;
+        this.grantAccessToUser = function(socket) {
+            // TODO make sure that this method is safe against multiple calls with the same socket argument
+
+            var clientId = this.nextClientId;
+            this.nextClientId++;
+
+            this.drawingData[clientId] = new Array();
+
+            var boardDirector = this;
+            if (clientId == 0) {        // this connection is with board creator
+                socket.on("set-password", function(newPassword) {
+                    if (isValidPassword(newPassowrd)) {
+                        boardDirector.password = newPassword;
+                        return;
+                    }
+                    socket.emit('invalid-password', 'The password must be at least 8 characters long!');
+                });
+            }
 
             socket.on("draw point", function(data_point, counter){
-                drawingData[clientId].push(data_point);
+                boardDirector.drawingData[clientId].push(data_point);
                 socket.broadcast.emit('draw point', data_point, counter);
             });
 
             socket.on("highlight", function(x, y){
                 socket.broadcast.emit('highlight', x, y, clientId);
-            });            
+            });
 
             socket.on('disconnect', function() {
                 socket.broadcast.emit('user left', clientId);
-            })
+            });
 
-            socket.emit("initialize", clientId, drawingData);
+            // the initialize event is only sent when the user is granted access to the board
+            socket.emit("initialize", clientId, this.drawingData);
 
             socket.broadcast.emit('welcome', clientId);
+        };
+
+        // persists current state to the database
+        this.saveToDB = function() {}; // TODO implement!
+        // populate current state with data from the database
+        this.loadFromDB = function() {}; // TODO implement!
+
+
+        //////////////////// method end definitions //////////////////////
+
+        var boardDirector = this;
+        // initialization
+        this.boardNameSpace.on('connection', function(socket){
+            console.log('someone connected on board ' + boardId);
+            boardDirector.verifyUser(socket);
         });
     }
 } // module.exports
