c7cb5126341f0a7ec1d7e719e8416693c84fa248
d87zhang <dzhang323@hotmail.com>
2016-11-18 21:41:02 -0500

    Add Comment class

---
+++ Comment.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Comment: function(authorClientId, message, xPos, yPos) {
+        this.authorClientId = authorClientId;
+        this.message = message;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
